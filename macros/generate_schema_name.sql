5f3507f6e948fa70705c1d4d9ea2734883aa51e4
preethik <66732076+preethik-cse@users.noreply.github.com>
2026-09-18 02:45:20 +0530

    Add production schema routing

---
+++ macros/generate_schema_name.sql
@@ -0,0 +1,11 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- if target.name == 'prod' and custom_schema_name is not none -%}
+        {{ custom_schema_name | trim }}
+
+    {%- else -%}
+        {{ target.schema }}
+
+    {%- endif -%}
+
+{%- endmacro %}
